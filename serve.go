cb7ef5ed9b81dcf328ba4ca02aeb87703fdc9d5d
Derek <derekstich@gmail.com>
2016-12-14 11:13:37 -0500

    attacheck the js cart plugin

--- serve.go
+++ serve.go
@@ -6,9 +6,20 @@ import (
 )
 
 func main() {
-	server := http.FileServer(http.Dir("public"))
-	http.Handle("/", server)
+	go func() {
+		server := http.FileServer(http.Dir("/home/dev/go/src/github.com/TeamFairmont/hugo/bookshelf/public"))
+		http.Handle("/", server)
+		http.Handle("/test", http.FileServer(http.Dir("/home/dev/go/src/github.com/TeamFairmont/hugo/bookshelf/public/test/")))
+		http.Handle("/dist", http.FileServer(http.Dir("/home/dev/go/src/github.com/TeamFairmont/hugo/bookshelf/public/test/dist/")))
 
-	log.Println("Listening...")
-	http.ListenAndServe(":8181", nil)
+		log.Println("Listening...")
+		http.ListenAndServe(":8181", nil)
+	}()
+	go func() {
+		//log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./public/test/"))))
+		//log.Fatal(http.ListenAndServe("/dist/", http.FileServer(http.Dir("./test/dist/"))))
+	}()
+
+	forever := make(chan bool)
+	<-forever
 }
